4e4965a301a2b3dd08a4d1abc8b278849e4d809a
Mehedi6023 <quazimehedihasanovi@gmail.com>
2025-07-13 13:14:04 +0600

    added modal for medicine details

--- src/Pages/CategoryDetails/CategoryDetails.jsx
+++ src/Pages/CategoryDetails/CategoryDetails.jsx
@@ -1,29 +1,46 @@
-import { useQuery } from '@tanstack/react-query'
-import React from 'react'
-import { useLocation } from 'react-router-dom'
-import useAxiosSecure from '../../hooks/useAxiosSecure'
-import MedicinesTable from './MedicinesTable'
+import { useQuery } from '@tanstack/react-query';
+import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import useAxiosSecure from '../../hooks/useAxiosSecure';
+import MedicinesTable from './MedicinesTable';
+import DetailsModal from './DetailsModal';
 
 const CategoryDetails = () => {
-  const { publicApi } = useAxiosSecure()
-  const location = useLocation()
+  const { publicApi } = useAxiosSecure();
+  const location = useLocation();
 
+  const searchParams = new URLSearchParams(location.search);
+  const category = searchParams.get('category');
 
-  const searchParams = new URLSearchParams(location.search)
-  const category = searchParams.get('category')
+  // get medicines
   const { data } = useQuery({
-    queryKey: ["medicines", category],
-    queryFn: async () => publicApi.get(`/medicines/${category}`)
-  })
-  const medicines = data?.medicines
+    queryKey: ['medicines', category],
+    queryFn: async () => publicApi.get(`/medicines/${category}`),
+  });
+  const medicines = data?.medicines;
+
+  // modal logic 👇
+  const [isOpen, setIsOpen] = useState(false)
+  const [selectedMedicine, setSelectedMedicine] = useState(null)
+
+  const openModal = (medicine) => {
+    setSelectedMedicine(medicine)
+    setIsOpen(true)
+  }
+
+  const closeModal = () => {
+    setIsOpen(false)
+    setSelectedMedicine(null)
+  }
+  // modal logic 👆
 
   return (
     <>
       <h1>{category}</h1>
-      <MedicinesTable medicines = {medicines} />
-
+      <MedicinesTable openModal={openModal} medicines={medicines} />
+      <DetailsModal isOpen={isOpen} close={closeModal} medicine={selectedMedicine} />
     </>
-  )
-}
+  );
+};
 
-export default CategoryDetails
+export default CategoryDetails;
--- src/Pages/CategoryDetails/MedicinesTable.jsx
+++ src/Pages/CategoryDetails/MedicinesTable.jsx
@@ -1,11 +1,10 @@
+import { Button } from '@headlessui/react';
 import React from 'react';
 
-const MedicinesTable = ({ medicines }) => {
-  console.log(medicines);
+const MedicinesTable = ({ medicines, openModal }) => {
   return (
     <div className="overflow-x-auto max-w-7xl mx-auto">
       <table className="table">
-        {/* head */}
         <thead>
           <tr>
             <th>
@@ -14,45 +13,78 @@ const MedicinesTable = ({ medicines }) => {
             <th>Name</th>
             <th>Company</th>
             <th>Price</th>
+            <th>Discount</th>
             <th>Action</th>
           </tr>
         </thead>
         <tbody>
-          {/* row 1 */}
-          {medicines &&
-            medicines.map((medicine, index) => (
-              <tr key={index}>
-                <th>
-                  <label>{index + 1}</label>
-                </th>
-                <td>
-                  <div className="flex items-center gap-3">
-                    <div className="avatar">
-                      <div className="mask mask-squircle h-12 w-12">
-                        <img
-                          src={medicine.image}
-                          alt="Avatar Tailwind CSS Component"
-                        />
-                      </div>
-                    </div>
-                    <div>
-                      <div className="font-bold">{medicine.itemName}</div>
-                      <div className="text-sm opacity-50">
-                        {medicine.genericName}
-                      </div>
+          {medicines?.map((medicine, index) => (
+            <tr key={index}>
+              <th>
+                <label>{index + 1}</label>
+              </th>
+              <td>
+                <div className="flex items-center gap-3">
+                  <div className="avatar">
+                    <div className="mask mask-squircle h-12 w-12">
+                      <img
+                        src={medicine.image}
+                        alt="Avatar Tailwind CSS Component"
+                      />
                     </div>
                   </div>
-                </td>
-                <td>{medicine.company}</td>
-                <td>${medicine.price}</td>
-                <td>
+                  <div>
+                    <div className="font-bold">{medicine.itemName}</div>
+                    <div className="text-sm opacity-50">
+                      {medicine.genericName}
+                    </div>
+                  </div>
+                </div>
+              </td>
+              <td>{medicine.company}</td>
+              <td>${medicine.price}</td>
+              <td>{medicine.discount}%</td>
+              <td>
                   <div className="join join-vertical">
-                    <button className="btn join-item">Button</button>
-                    <button className="btn join-item">Button</button>
-                  </div>
-                </td>
-              </tr>
-            ))}
+                    <Button
+                      onClick={() => openModal(medicine)}
+                      className="rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:bg-black/30 btn join-item flex items-center gap-2"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-4 h-4"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8Z" />
+                        <circle cx="12" cy="12" r="3" />
+                      </svg>
+                  Details
+                    </Button>
+                    <button className="btn join-item flex items-center gap-2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-4 h-4"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <circle cx="9" cy="21" r="1" />
+                        <circle cx="20" cy="21" r="1" />
+                        <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6" />
+                      </svg>
+                      Add to cart
+                </button>
+                  </div>              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
